7122ec17b3ec20595d99cc643fd28e9d210a689c
salman <salman.ansari776@gmail.com>
2026-05-06 19:19:05 +0530

    adding bazerl build

---
+++ bazel-ops/calculator/calculator_test.py
@@ -0,0 +1,16 @@
+import unittest
+from calculator import Calculator
+
+class CalculatorTest(unittest.TestCase):
+    def test_add(self):
+        val = Calculator().add(2, 3)
+        self.assertEqual(Calculator().add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(5 - 2, 3)
+
+    def test_multiply(self):
+        self.assertEqual(4 * 3, 12)
+
+    def test_divide(self):
+        self.assertEqual(10 / 2, 5)
